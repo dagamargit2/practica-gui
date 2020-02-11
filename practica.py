1a5e7181aefc9ad416c03d695cff6bd576ee0412
Daniel Garrido <dgarrido@lcc.uma.es>
2020-02-11 10:55:42 +0100

    lectura temp en entry

--- practica.py
+++ practica.py
@@ -1,8 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
+from sense_emu import SenseHat  # pip3 install sense_emu
 
 class Aplicacion:
     def __init__(self):
+        self.sense = SenseHat()
+        #  temp = sense.temp
+
         self.ventana1=tk.Tk()
         self.labelframe1=ttk.LabelFrame(self.ventana1, text="Medidas")        
         self.labelframe1.grid(column=0, row=0)        
@@ -27,22 +31,30 @@ class Aplicacion:
     def mediciones(self):
         self.label1=ttk.Label(self.labelframe1, text="Temperatura:")
         self.label1.grid(column=0, row=0, padx=4, pady=4)
-        self.entry1=ttk.Entry(self.labelframe1)
-        self.entry1.grid(column=1, row=0, padx=4, pady=4)
+        
+        self.datoTemp=tk.StringVar()
+        self.entryTemp=ttk.Entry(self.labelframe1,textvariable=self.datoTemp)
+        self.entryTemp.grid(column=1, row=0, padx=4, pady=4)
         self.label2=ttk.Label(self.labelframe1, text="Presión:")        
         self.label2.grid(column=0, row=1, padx=4, pady=4)
         self.entry2=ttk.Entry(self.labelframe1)
         self.entry2.grid(column=1, row=1, padx=4, pady=4)
-        self.label3=ttk.Label(self.labelframe1, text="Humedad")
+        self.label3=ttk.Label(self.labelframe1, text="Humedad:")
         self.label3.grid(column=0, row=2, padx=4, pady=4)
         self.entry3=ttk.Entry(self.labelframe1)
         self.entry3.grid(column=1, row=2, padx=4, pady=4)
 
 
     def operaciones(self):
-        self.boton2=ttk.Button(self.labelframe2, text="Lectura")
+        self.boton2=ttk.Button(self.labelframe2, text="Lectura", command=self.medir)
         self.boton2.grid(column=0, row=0, padx=4, pady=4)
         self.boton3=ttk.Button(self.labelframe2, text="Añadir")
         self.boton3.grid(column=1, row=0, padx=4, pady=4)
+
+    def medir(self):
+        self.entryTemp.delete(0,tk.END)
+        self.entryTemp.insert(0,str(self.sense.temp))
+        print(self.datoTemp)
+
 
 aplicacion1=Aplicacion()
