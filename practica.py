f828eccbdb84b30ae05aae285ccfc1aa2ca3ca7e
Daniel Garrido <dgarrido@lcc.uma.es>
2020-02-16 21:13:19 +0100

    primera página añadida y algunas refactorizaciones

--- practica.py
+++ practica.py
@@ -20,27 +20,17 @@ class Aplicacion:
         #  temp = sense.temp
 
         self.ventana1=tk.Tk()
-        self.labelframe1=ttk.LabelFrame(self.ventana1, text="Medidas")        
-        self.labelframe1.grid(column=0, row=0)        
+        # self.ventana1.title("Prueba del control Notebook")
+
+        self.cuaderno1 = ttk.Notebook(self.ventana1)
+        self.pagina1 = ttk.Frame(self.cuaderno1)
+        self.cuaderno1.add(self.pagina1, text="Monitorización")
+        self.cuaderno1.grid(column=0, row=0)        
 
         self.mediciones()
-
-        self.canvas1=tk.Canvas(self.ventana1, width=self.ANCHO_CANVAS, height=self.ALTO_CANVAS, background="black")
-        self.canvas1.grid(column=1, row=0,rowspan = 2)
-        self.columna_cursor = 0
-        self.fila_cursor = 0
-
-        self.crear_cuadrado()        
-
-        self.labelframe2=ttk.LabelFrame(self.ventana1, text="Operaciones")        
-        self.labelframe2.grid(column=0, row=1)        
+        self.canvas()
         self.operaciones()
-
-        self.labelframe3=ttk.LabelFrame(self.ventana1, text="Histórico")        
-        self.labelframe3.grid(column=0, row=2, columnspan = 2, sticky=tk.W+tk.E)        
-
-        self.listbox1=tk.Listbox(self.labelframe3)
-        self.listbox1.pack(side = tk.LEFT, fill = tk.BOTH, expand = True)
+        self.historico()
 
         self.ventana1.bind("<KeyPress>", self.presion_tecla)
         self.ventana1.bind("<Button-1>", self.presion_raton)
@@ -56,6 +46,9 @@ class Aplicacion:
         self.ventana1.mainloop()
 
     def mediciones(self):
+        self.labelframe1=ttk.LabelFrame(self.pagina1, text="Medidas")        
+        self.labelframe1.grid(column=0, row=0)        
+
         self.label1=ttk.Label(self.labelframe1, text="Temperatura:")
         self.label1.grid(column=0, row=0, padx=4, pady=4)
         
@@ -71,13 +64,37 @@ class Aplicacion:
         self.entry3=ttk.Entry(self.labelframe1)
         self.entry3.grid(column=1, row=2, padx=4, pady=4)
 
+    def canvas(self):
+        self.canvas1=tk.Canvas(self.pagina1, width=self.ANCHO_CANVAS, height=self.ALTO_CANVAS, background="black")
+        self.canvas1.grid(column=1, row=0,rowspan = 2)
+        self.columna_cursor = 0
+        self.fila_cursor = 0
+
+        self.crear_cuadrado()        
+
 
     def operaciones(self):
+        self.labelframe2=ttk.LabelFrame(self.pagina1, text="Operaciones")        
+        self.labelframe2.grid(column=0, row=1)        
+
         self.boton2=ttk.Button(self.labelframe2, text="Lectura", command=self.medir)
         self.boton2.grid(column=0, row=0, padx=4, pady=4)
         self.boton3=ttk.Button(self.labelframe2, text="Añadir")
         self.boton3.grid(column=1, row=0, padx=4, pady=4)
 
+    def historico(self):
+        self.labelframe3=ttk.LabelFrame(self.pagina1, text="Histórico")        
+        self.labelframe3.grid(column=0, row=2, columnspan = 2, sticky=tk.W+tk.E)        
+
+        self.listbox1=tk.Listbox(self.labelframe3)
+        self.listbox1.pack(side = tk.LEFT, fill = tk.BOTH, expand = True)
+
+
+    # Esta operación crea un cuadrado relleno según la temperatura en la posición
+    # actual del cursor.
+    # Actualiza también el cuadro de texto con la temperatura
+    #
+    # TODO: Comprobar si llamadas continuas a create_rectangle agotan recursos
     def medir(self):
         self.datoTemp.set(str(self.sense.temp))
         #print(self.datoTemp.get())
@@ -88,7 +105,9 @@ class Aplicacion:
                                                     (self.fila_cursor+1)*self.ALTO_CURSOR,
                                                     fill = self.color(float(self.datoTemp.get())))        
         self.canvas1.tag_raise(self.cuadrado)                                                    
-    
+
+    # Esta función utiliza las coordenadas actuales del cursor para crear el
+    # cuadrado con marco rojo    
     def crear_cuadrado(self):
         self.cuadrado=self.canvas1.create_rectangle(self.columna_cursor*self.ANCHO_CURSOR,
                                                     self.fila_cursor*self.ALTO_CURSOR,
